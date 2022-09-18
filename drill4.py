c70cddccb5aa26bc586baf7eda86cad45d3182bf
nohc2001 <nohc2001@naver.com>
2022-09-18 17:37:43 +0900

    Finish DRILL4

--- drill4.py
+++ drill4.py
@@ -1,21 +1,21 @@
-import turtle;
+import turtle
 
 turtle.reset()
 
-index = 0;
+index = 0
 while (index < 6):
-    turtle.penup();
-    turtle.goto(-200 + index * 100, -200);
-    turtle.pendown();
-    turtle.goto(-200 + index * 100, 300);
-    index += 1;
+    turtle.penup()
+    turtle.goto(-200 + index * 100, -200)
+    turtle.pendown()
+    turtle.goto(-200 + index * 100, 300)
+    index += 1
 
-index = 0;
+index = 0
 while (index < 6):
-    turtle.penup();
-    turtle.goto(-200, -200 + index * 100);
-    turtle.pendown();
-    turtle.goto(300, -200 + index * 100);
-    index += 1;
+    turtle.penup()
+    turtle.goto(-200, -200 + index * 100)
+    turtle.pendown()
+    turtle.goto(300, -200 + index * 100)
+    index += 1
 
 turtle.exitonclick()
